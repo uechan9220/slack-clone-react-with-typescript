c0ce4cffcbc0953efaa6e95982b7fdecbd30c582
mokeeemokeee <moke.0922.moke@gmail.com>
2020-02-11 01:22:29 +0900

    MessageBox style fix

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,8 +1,78 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-export function MessageBox(){
-  return(
-    <div>test</div>
+const Container = styled.div`
+  margin-top: 85px;
+  li {
+    margin: 0.5rem 0;
+  }
+  p {
+    margin-top: .25px;
+  }
+`
+
+const UserName = styled.span`
+  font-weight: 800;
+  margin-right: 5px;
+  font-size: 1.2rem;
+`
+
+const DateSpan = styled.span`
+  color: darkgray;
+`
+
+export function MessageBox() {
+  const message = [
+    {
+      message:
+        '今夜は月がきれいですね。ですが僕は月よりも寿司が好きです。ちなみに今日誕生日の人いますか？',
+      user: 'boku_doraemoon',
+      date: 'Sat Feb 11 2020 12:00:00 GMT+0900 (CEST)'
+    },
+    {
+      message:
+        '今夜は月がきれいですね。ですが僕は月よりも寿司が好きです。ちなみに今日誕生日の人いますか？',
+      user: 'boku_doraemoon',
+      date: 'Sat Feb 11 2020 12:00:00 GMT+0900 (CEST)'
+    },
+    {
+      message:
+        '今夜は月がきれいですね。ですが僕は月よりも寿司が好きです。ちなみに今日誕生日の人いますか？',
+      user: 'boku_doraemoon',
+      date: 'Sat Feb 11 2020 12:00:00 GMT+0900 (CEST)'
+    },
+    {
+      message:
+        '今夜は月がきれいですね。ですが僕は月よりも寿司が好きです。ちなみに今日誕生日の人いますか？',
+      user: 'boku_doraemoon',
+      date: 'Sat Feb 11 2020 12:00:00 GMT+0900 (CEST)'
+    },
+    {
+      message:
+        '今夜は月がきれいですね。ですが僕は月よりも寿司が好きです。ちなみに今日誕生日の人いますか？',
+      user: 'boku_doraemoon',
+      date: 'Sat Feb 11 2020 12:00:00 GMT+0900 (CEST)'
+    },
+    {
+      message:
+        '今夜は月がきれいですね。ですが僕は月よりも寿司が好きです。ちなみに今日誕生日の人いますか？',
+      user: 'boku_doraemoon',
+      date: 'Sat Feb 11 2020 12:00:00 GMT+0900 (CEST)'
+    }
+  ]
+  return (
+    <Container>
+      <ul>
+        {message.map((message, index) => (
+          <li key={index}>
+            <UserName>{message.user}</UserName>
+            <DateSpan>
+              {new Intl.DateTimeFormat('en-GB').format(new Date(message.date))}
+            </DateSpan>
+            <p>{message.message}</p>
+          </li>
+        ))}
+      </ul>
+    </Container>
   )
 }
