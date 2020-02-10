7c1dab5f84cf9a6cd84be4d9ed43cb7368d0a4e5
mokeeemokeee <moke.0922.moke@gmail.com>
2020-02-11 01:32:25 +0900

    input style fix

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
-export function InputMessage(){
-  return(
-    <input type='text' />
-  )
+const InputStyle = styled.input`
+  padding: 1rem;
+  border-radius: 7px;
+  border: 3px solid darkgray;
+  font-size: 1rem;
+  outline: none;
+  &:hover,
+  &:active,
+  &:focus {
+    border: 3px solid dimgray;
+  }
+`
+
+export function InputMessage() {
+  return <InputStyle type='text' placeholder='Message boku_doraemoon' />
 }
