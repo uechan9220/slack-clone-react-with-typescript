a99ed08ae27e11ea75fee38596ff20290b0661e3
mokeeemokeee <moke.0922.moke@gmail.com>
2020-02-26 02:41:28 +0900

    Form Input add

--- src/components/Sidebar/Channels/JoinChannel.component.tsx
+++ src/components/Sidebar/Channels/JoinChannel.component.tsx
@@ -4,13 +4,14 @@ import { Form } from '../../../styles/ModalButtons'
 import { allChannelsQuery } from '../../../data/queries'
 import { Query, QueryResult } from 'react-apollo'
 import styled from 'styled-components'
+import { Input } from '../../../styles/Input.styles'
 
 interface Props {
   exitCallback: () => void
 }
 
 const ChannelContainer = styled.div`
-  padding: 1rem;
+  padding: 1rem 2rem;
   border-top: 1px solid ${props => props.theme.borderColorLight};
 `
 
@@ -23,11 +24,29 @@ export function JoinChannel(props: Props) {
             return <p>loading</p>
           }
 
-          return data.Channel.map((channel: { id: string; name: string }) => (
-            <ChannelContainer key={channel.id}>
-              # {channel.name}
-            </ChannelContainer>
-          ))
+          return (
+            <>
+              <Form
+                onSubmit={(e: any) => {
+                  console.log(e.target.channelName.value)
+                  e.preventDefault()
+
+                  e.target.reset()
+                }}
+              >
+                <Input
+                  name="channelName"
+                  id="channelName"
+                  placeholder="Seach channels"
+                />
+              </Form>
+              {data.Channel.map((channel: { id: string; name: string }) => (
+                <ChannelContainer key={channel.id}>
+                  # {channel.name}
+                </ChannelContainer>
+              ))}
+            </>
+          )
         }}
       </Query>
     </Modal>
